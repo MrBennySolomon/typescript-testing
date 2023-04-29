88da44ca9cd9256c2ab1f115ab7b4da190d7ad3e
MrBennySolomon <MrBennySolomon@gmail.com>
2023-04-29 15:47:50 +0300

    typescript-testing

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './TodoItem.module.css';
 
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className='item'>{props.text}</li>
+const TodoItem: React.FC<{ text: string; onRemoveTodo: (event: React.MouseEvent) => void }> = (props) => {
+  return <li className='item' onClick={props.onRemoveTodo}>{props.text}</li>
 };
 
 export default TodoItem;
